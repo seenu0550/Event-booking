c57107d55e999fb2d98634cf1dd767dbe58d965a
seenu0550 <g.jssai17@gmail.com>
2026-08-11 12:06:12 +0530

    Fixed all urls

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'https://event-booking-1-ulji.onrender.com'
+        'https://event-booking-frontend-na2w.onrender.com'
     ],
     credentials: true
 }));
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://event-booking-b6hl.onrender.com/api'
+  baseURL: 'https://event-booking-wweu.onrender.com/api'
 });
 
 API.interceptors.request.use((config) => {
